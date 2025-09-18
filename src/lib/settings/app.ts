14105ac6b9c24c6995ef91f3dc3b7b320dddeefc
kayoe <kayoe279@gmail.com>
2025-09-18 14:14:07 +0800

    fix app config title

--- src/lib/settings/app.ts
+++ src/lib/settings/app.ts
@@ -60,7 +60,7 @@ export const defaultLoginParams = {
 };
 
 export const appConfig = {
-  title: "Kairo Admin React",
+  title: "Kairo Admin",
   description:
     "Kairo Admin React 是一个基于 React 19、React Router 7、Vite、TypeScript 和 Antd 的中后台解决方案，它可以帮助你快速搭建企业级中后台项目，持续更新中。",
   avatar: avatarImage,
